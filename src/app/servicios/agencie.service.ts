57d42855bae98503cba2e23b2d4b2b47f8b14111
ebazan <ezequiel.bazan@mercadolibre.com>
2019-07-25 14:51:06 -0300

    se aplicacan validaciones de ng lint

--- src/app/servicios/agencie.service.ts
+++ src/app/servicios/agencie.service.ts
@@ -10,55 +10,55 @@ import { Agencie } from '../models/agencie';
   providedIn: 'root'
 })
 export class AgencieService {
-  
+
   public url: string;
-  
+
   constructor(private _http: Http) {
     this.url = environment.baseUrl;
   }
 
   getAgencies(formGroup: FormGroup) {
-    let headers = new Headers({ 
+    const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    let agencie = formGroup.value;
-    let options = new RequestOptions({ headers: headers });
-    let urlAPI = this.url + 'MLA/payment_methods/rapipago/agencies?'
+    const agencie = formGroup.value;
+    const options = new RequestOptions({ headers: headers });
+    const urlAPI = this.url + 'MLA/payment_methods/rapipago/agencies?'
                           + '&lat=' + agencie.latitud + '&lon=' + agencie.longitud + '&radius=' + agencie.radio
-                          + '&limit=' + agencie.limit + "&offset=" + agencie.offset + "&orderby=" + agencie.orderby
-                          + "&criterio=" + agencie.criterio;
+                          + '&limit=' + agencie.limit + '&offset=' + agencie.offset + '&orderby=' + agencie.orderby
+                          + '&criterio=' + agencie.criterio;
     return this._http.get(urlAPI, options).pipe(map(res => res.json()));
   }
 
   getAgenciesRecomended() {
-    let headers = new Headers({ 
+    const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    let options = new RequestOptions({ headers: headers });
+    const options = new RequestOptions({ headers: headers });
     return this._http.get(this.url + 'agencies-recomended', options).pipe(map(res => res.json()));
   }
 
   getAgencie(id: number) {
-    let headers = new Headers({ 
+    const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    let options = new RequestOptions({ headers: headers });
+    const options = new RequestOptions({ headers: headers });
     return this._http.get(this.url + 'agencies/' + id, options).pipe(map(res => res.json()));
   }
 
   saveAgencie(agencie: Agencie) {
-    let headers = new Headers({ 
+    const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    let options = new RequestOptions({ headers: headers });
+    const options = new RequestOptions({ headers: headers });
     return this._http.post(this.url + 'agencies', agencie, options).pipe(map(res => res.json()));
   }
 
   removeAgencie(id: number) {
-    let headers = new Headers({ 
+    const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    let options = new RequestOptions({ headers: headers });
+    const options = new RequestOptions({ headers: headers });
     return this._http.delete(this.url + 'agencies/' + id, options).pipe(map(res => res.json()));
   }
 
